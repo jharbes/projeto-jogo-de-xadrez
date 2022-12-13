bb959680c2d14fc0282b7a54967bc80b26e49ae0
Jorge Harbes <jharbes@hotmail.com>
2022-12-13 13:31:34 -0300

    "Limpando a tela e tratando as excecoes

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,7 +29,13 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-
+	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner teclado) {
 		try {
 			String s = teclado.nextLine();
@@ -37,7 +43,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Erro lendo posição de xadrez, valores válidos são de A1 até H8.");
+			throw new InputMismatchException("Erro lendo posicao de xadrez, valores validos sao de a1 ate' h8.");
 		}
 	}
 
