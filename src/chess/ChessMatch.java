fa245e926be98be94c63b1286a1292d3b55fdec1
Jorge Harbes <jharbes@hotmail.com>
2022-12-14 08:34:01 -0300

    Implementando movimentos possiveis da torre PARTE 2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -45,6 +46,12 @@ public class ChessMatch {
 			throw new ChessException("Nao existe peca na posicao de origem");
 		if (!this.board.piece(position).isThereAnyPossibleMove())
 			throw new ChessException("Nao existem movimentos possiveis para a peca escolhida.");
+	}
+	
+	private void validateTargetPosition(Position source,Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peca escolhida nao pode se mover para o destino solicitado.");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
