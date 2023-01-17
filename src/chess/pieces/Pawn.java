0532bfa6faeda55f55518030d541ec18c31a4804
Jorge Harbes <jharbes@hotmail.com>
2023-01-17 12:02:42 -0300

    implementacao da peça peão parte 2

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -41,9 +41,50 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 
 			}
+			
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (this.getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+
+			}
+		}
+		else {
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+
+			}
+
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+
+			if (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)
+					&& this.getBoard().positionExists(p2) && !this.getBoard().thereIsAPiece(p2)
+					&& this.getMoveCount() == 0) {
+				// testando o caso de ser a primeira jogada a possibilidade de andar duas casas
+
+				mat[p.getRow()][p.getColumn()] = true;
+
+			}
+			
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (this.getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+
+			}
+			
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (this.getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+
+			}
 		}
 
-		return null;
+		return mat;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 }
